aef6710c608e2b403a9d3eb5cdd78cde6d0e7a85
Huynh Trung Nguyen <104974403+niyya123@users.noreply.github.com>
2025-08-06 15:05:46 +0700

    fix auth mongo

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ var configValues = require('./config.json')
 
 module.exports ={
     getDbConnectionString:function(){
-        return `mongodb+srv://${configValues.username}:${configValues.password}%40@cluster0.huynvld.mongodb.net/`
+        return `mongodb+srv://${configValues.username}:${configValues.password}@cluster0.huynvld.mongodb.net/`
     }
 }
